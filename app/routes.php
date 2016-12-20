0b6942edcf291ccfb115b5e343c6cad50c7861e9
Bas Broek <bas@basbroek.nl>
2016-12-20 18:30:25 +0100

    Finish tweet creation

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,11 @@
 <?php
 
-Route::post('tweets/create', function($authorID, $content, $timestamp) {
-	DB::insert('insert into tweets (authorID, content, timestamp) values (?, ?, ?)', [$authorID, $content, $timestamp]);
+Route::post('tweets/create', function() {
+	$authorID = Input::get('authorID');
+	$content = Input::get('content');
+	return DB::table('tweets')->insertGetId(
+    	['authorID' => $authorID, 'content' => $content]
+	);
 });
 
 Route::get('tweets', function() {
@@ -12,7 +16,8 @@ Route::get('tweets', function() {
 Route::get('tweets/{id}', function($id) {
     $tweet = DB::select('select * from tweets where id = :id', ['id' => $id]);
 	return json_encode($tweet);
-});
+})
+->where('id', '[0-9]+');;
 
 Route::get('users', function() {
     $users = DB::select('select * from users');
@@ -22,4 +27,15 @@ Route::get('users', function() {
 Route::get('users/{id}', function($id) {
     $user = DB::select('select * from users where id = :id', ['id' => $id]);
 	return json_encode($user);
-});
+})
+->where('id', '[0-9]+');;
+
+Route::post('users/follow/{id}', function($id) {
+
+})
+->where('id', '[0-9]+');;
+
+Route::post('users/unfollow/{id}', function($id) {
+
+})
+->where('id', '[0-9]+');;
